138f96bfc859305b04c66f3ba3deafa8902520be
Gaël Louage <gaellouage@hotmail.com>
2025-06-07 12:13:03 +0200

    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.

---
+++ ExesandOhsExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static bool XOLinq(string input)
+    {
+        var oS = input.Count(x => char.ToLower(x) == 'o');
+        var xS = input.Count(x => char.ToLower(x) == 'x');
+        return oS == xS;
+    }
+    public static bool XO(string input)
+    {
+        var oS = 0;
+        var xS = 0;
+        foreach (var x in input)
+        {
+            GetOsAndXs(ref oS, ref xS, x);
+        }
+        return oS == xS;
+    }
+
+    private static void GetOsAndXs(ref int oS, ref int xS, char x)
+    {
+        switch (char.ToLower(x))
+        {
+            case 'o':
+                oS++;
+                break;
+            case 'x':
+                xS++;
+                break;
+        }
+    }
+}
